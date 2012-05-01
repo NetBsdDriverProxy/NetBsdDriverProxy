2892a724f0ce7a6018d8ce4f7a76b00d826cb632
guest <guest@localhost.localdomain>
2012-04-30 22:28:24 -0500

    ...

--- src_analyze/Analyze.java
+++ src_analyze/Analyze.java
@@ -1,18 +1,39 @@
 import java.io.*;
+import java.util.*;
+import java.util.regex.*;
 
 class Analyze {
-	static byte[] buf = new byte[4096];
+	static char[] buf = new char[4096];
+	static Pattern p = Pattern.compile("#.*$", Pattern.CASE_INSENSITIVE);
 	public static void main(String[] args) throws FileNotFoundException, IOException {
 		for(String fname : args) {
-			FileInputStream fis = new FileInputStream(fname);
+			FileReader in = new FileReader(fname);
+			BufferedWriter out = new BufferedWriter(new FileWriter("config.h"));
+				out.write("ABC");
+				out.write("ABC");
 			
 			int len;
 			StringBuilder sb = new StringBuilder();
-			while ((len = fis.read(buf)) > 0) {
+			while ((len = in.read(buf)) > 0) {
 				sb.append(buf);
 			}
 			
-			//System.out.println(sb.length());
+			Matcher m = p.matcher(sb.toString());
+			//System.out.println(m.matches());
+			//System.out.println(sb.toString());
+			
+			LinkedList<String> l = new LinkedList<String>();
+			while(m.find()) {
+				l.add(m.group());
+			}
+			
+			for(String s : l) {
+				out.write(s);
+				out.write("\n");
+			}
+			
+			out.close();
+			in.close();
 		}
 	}
 }
